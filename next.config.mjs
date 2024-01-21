4443773d9fb3c57544562df531ef94c05a84ff7f
milyasbpa <basilyas.2015@gmail.com>
2024-01-21 10:33:11 +0700

    fix: add pwa

--- next.config.mjs
+++ next.config.mjs
@@ -9,5 +9,11 @@ const nextConfig = {
     ];
   },
 };
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
 
-export default nextConfig;
+export default withPWA(nextConfig);
